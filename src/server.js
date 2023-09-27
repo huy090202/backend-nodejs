ec350678fec52defd983045dc70452b069cb7711
huy090202 <pham.dang.huy092@gmail.com>
2023-09-27 22:12:42 +0700

    27/9

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
+require("dotenv").config(); // Co su dung process.env thi phai de require("dotenv").config() tren cung
 const express = require("express"); // commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/database");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888;
@@ -12,6 +13,13 @@ configViewEngine(app);
 
 // Khai bao route
 app.use("/", webRoutes);
+
+// Test connection
+
+// simple query
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+  console.log(">>>results= ", results); // results contains rows returned by server
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port} ${hostname}`);
